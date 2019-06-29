3a4e84d058e6e88e9474b5b0aa24b133a3225290
John Melendy <melja@pobox.com>
2019-06-29 13:57:51 -0700

    add initial files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import { Key } from './mykeys.js';
+import './styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(function() {
+  let myKey = new Key();
+  console.log(myKey);
+});
